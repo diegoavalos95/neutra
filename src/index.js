0836dc93b27bbc482a045a4b5c291a944164fe66
alexisllamas <alexisnavarro23@gmail.com>
2017-05-31 22:38:12 +0000

    Add react-router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom'
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Main = () => (
+  <Router>
+    <Route exact path="/" component={App}/>
+  </Router>
+)
+
+ReactDOM.render(<Main />, document.getElementById('root'));
 registerServiceWorker();
